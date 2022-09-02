fbef7ef85916f890642825ab61748b1bdf3a1542
Abzalitdinova <abzalitdinova@mail.ru>
2022-09-02 15:55:22 +0300

    ДЗ к сминару 9

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("введите первое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число, больше, чем первое");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int SummNubmers (int start, int end)
+ {
+    if (start == end) return end;
+    return (start + SummNubmers(start+1,end));
+ }
+
+Console.WriteLine (SummNubmers(N,M));
